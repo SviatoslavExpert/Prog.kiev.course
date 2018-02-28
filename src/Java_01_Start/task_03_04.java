91eebdb73c367a65c52fa5e8aeca05d9e3a6c67e
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-28 18:25:31 +0200

    task

---
+++ src/Java_01_Start/task_03_04.java
@@ -0,0 +1,38 @@
+/*
+Java Start Task 03.04
+С помощью цикла нарисовать такую фигуру
+*
+**
+***
+****
+***
+**
+*
+ */
+
+package Java_01_Start;
+
+public class task_03_04 {
+
+    public static void main(String []args){
+        for (int i = 1; i < 6; i++){
+            switch(i){
+                case 1:
+                    System.out.println("*");
+                    break;
+                case 2:
+                    System.out.print("*"); System.out.println("*");
+                    break;
+                case 3:
+                    System.out.print("*"); System.out.print("*"); System.out.println("*");
+                    break;
+                case 4:
+                    System.out.print("*"); System.out.println("*");
+                    break;
+                case 5:
+                    System.out.println("*");
+                    break;
+            }
+        }
+    }
+}
