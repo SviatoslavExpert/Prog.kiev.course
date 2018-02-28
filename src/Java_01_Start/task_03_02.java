106d1fa618190c6ad9fe2b4adb83dd24ed347cef
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-28 18:22:46 +0200

    task

---
+++ src/Java_01_Start/task_03_02.java
@@ -0,0 +1,27 @@
+/*
+Java Start Task 03.02
+
+Есть круг с центром в начале координат и радиусом 4. Пользователь
+вводит с клавиатуры координаты точки x и y. Написать программу которая
+определит лежит ли эта точка внутри круга или нет.
+ */
+
+package Java_01_Start;
+
+import java.util.Scanner;
+
+public class task_03_02 {
+
+    public static void main (String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите значение координаты X точки:");
+        double x = sc.nextDouble();
+        System.out.println("Введите значение координаты Y точки:");
+        double y = sc.nextDouble();
+        double result = Math.sqrt((x * x) + (y * y));
+
+        if (result > 4) System.out.println("Точка лежит вне круга.");
+        else System.out.println("Точка лежит внутри круга.");
+        sc.close();
+    }
+}
