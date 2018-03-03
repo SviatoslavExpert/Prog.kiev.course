9e59ca25b50b83ea8008b96a5103ebf483916f36
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-03 13:54:11 +0200

    task

--- src/Java_01_Start/task_04_01.java
+++ src/Java_01_Start/task_04_01.java
@@ -17,8 +17,56 @@
 1 1 1 1 1 1
 1 1 1 1 1 1)
  */
-
 package Java_01_Start;
 
 public class task_04_01 {
+
+    public static void main(String[] args) {
+// TODO Auto-generated method stub
+        int[][]a = new int[5][5];
+        for (int i = 0; i < a.length; i++) {
+            for(int j = 0; j < a[i].length; j++){
+                a[i][j] = j+1;
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        rotateArray(a);
+    }
+
+    //   ROTATE
+    static void rotateArray(int[][]a){
+        for (int i = 0; i < a.length/2; i++) {
+            for (int j = i; j < a.length-1-i; j++) {
+                int b = a[i][j];
+                a[i][j] = a[a.length - j - 1][i];
+                a[a.length-j-1][i]=a[a.length-i-1][a.length-j-1];
+                a[a.length-i-1][a.length-j-1]=a[j][a.length-i-1];
+                a[j][a.length-i-1]=b;
+            }
+        }
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
 }
+
+/*
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = i + j;
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+ */
--- src/Java_01_Start/task_04_02.java
+++ src/Java_01_Start/task_04_02.java
@@ -10,16 +10,23 @@ import java.util.Arrays;
 public class task_04_02 {
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4};
+        //int[] arr = {1,2,3,4};
+        int[] arr = new int[4];
+        int x = 1;
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = x;
+            x++;
+        }
         System.out.println(Arrays.toString(arr));
+
         rotate(arr);
     }
 
     static void rotate(int[]a){
         for (int i = 0; i < a.length/2; i++) {
             int b = a[i];
-            a[i] = a[a.length-1-i];
-            a[a.length-i-1] = b;
+            a[i] = a[a.length - 1 - i];
+            a[a.length - i - 1] = b;
         }
         System.out.println(Arrays.toString(a));
     }
