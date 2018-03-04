940b6db20f48ed457d8cfb5cf555c2cc81f1012f
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-04 08:45:19 +0200

    task

---
+++ src/Java_00_Extra/Example02.java
@@ -0,0 +1,24 @@
+
+
+package Java_00_Extra;
+
+public class Example02 {
+
+    public static void main(String[] args) {
+        int[] a={1,2,3,4,5,67,8};
+        System.out.println("Значение переменной в главном методе до вызова sum - ");
+        for(int k:a){System.out.print(k+" ");}
+        System.out.println();
+
+        sum(a);
+
+        System.out.println();
+        System.out.println("Значение переменной в главном методе после вызова sum - ");
+        for(int k:a){System.out.print(k+" ");}
+    }
+    static void sum(int[]a){
+        a[3] = a[3] + 3;
+        System.out.println("Значение а в методе sum - ");
+        for(int k:a){System.out.print(k+" ");}
+    }
+}
