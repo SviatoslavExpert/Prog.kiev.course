0b0f0f6d3ec95da0029bdac5bebc9edfebb7c1f9
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-04 09:31:38 +0200

    task

--- src/Java_01_Start/task_05_02.java
+++ src/Java_01_Start/task_05_02.java
@@ -9,4 +9,42 @@
 package Java_01_Start;
 
 public class task_05_02 {
+
+    static int ObratChislo(int n) //метод для получения "обратного" числа - для 1234 вернет 4321
+    {
+        int obr = 0;
+        while (n > 0)
+        {
+            obr = 10 * obr + n % 10;
+            n /= 10;
+        }
+        return obr;
+    }
+    static void Main(String[] args)
+    {
+        int otv = 0;
+        int[] m = new int[999 * 999]; //в этом массиве будут все палиндромы
+        int u = 0;
+        for (int i = 999; i > 1; --i)
+        {
+            for (int j = 999; j > 1; --j)
+            {
+                otv = i * j; // то самое произведения
+                if (otv == ObratChislo(otv)) // проверяем на "палиндромность"
+                {
+                    m[u] = otv; //пишем в массив
+                    ++u;        //увеличиваем индекс массива
+                    break;      //завершаем цикл
+                }
+            }
+        }
+        otv = m[0];   //ищем макс. элемент массива
+        for (int i = 1; i < m.length; ++i)
+        {
+            if (m[i]> otv) otv = m[i];
+        }
+        //Console.WriteLine(otv);
+        //Console.ReadKey();
+        System.out.println(otv);
+    }
 }
