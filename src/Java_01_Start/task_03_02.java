0e07bcbf2524424fcdfaec89c24b577672e0e349
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-05 10:30:11 +0200

    task

--- src/Java_01_Start/task_03_02.java
+++ src/Java_01_Start/task_03_02.java
@@ -15,15 +15,19 @@ public class task_03_02 {
     public static void main (String []args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите значение координаты X точки:");
-        double x = sc.nextDouble();
+        double pointX = sc.nextDouble();
         System.out.println("Введите значение координаты Y точки:");
-        double y = sc.nextDouble();
+        double pointY = sc.nextDouble();
 
-        double result = Math.sqrt((x * x) + (y * y));
+        double circleX = 0;
+        double circleY = 0;
         double radius = 4;
 
-        if (result > radius) System.out.println("Точка лежит вне круга.");
-        else System.out.println("Точка лежит внутри круга.");
+        double result = Math.sqrt(Math.pow((pointX - circleX), 2) + Math.pow((pointY - circleY), 2));
+
+        if (result < radius) System.out.println("Точка лежит внутри круга.");
+        else if(result == radius) System.out.println("Точка лежит на линии круга.");
+        else System.out.println("Точка лежит вне круга.");
         sc.close();
     }
 }
