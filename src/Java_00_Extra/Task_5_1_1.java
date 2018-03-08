076e95e65fc888b5843768aabed8f3c3e9946e46
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-08 20:08:09 +0200

    task

--- src/Java_00_Extra/Task_5_1_1.java
+++ src/Java_00_Extra/Task_5_1_1.java
@@ -11,21 +11,22 @@ import java.util.Scanner;
 public class Task_5_1_1 {
 
     public static void main (String []args){
+
+        // Creating, filling and printing an array
         Scanner sc = new Scanner(System.in);
         int []arr = new int[5];
         System.out.println("Введите 5 чисел:");
-
-        for(int i = 0; i < 5; i++){
-            arr[i] = sc.nextInt();
-        }
+        for(int i = 0; i < 5; i++){ arr[i] = sc.nextInt();}
         System.out.print(Arrays.toString(arr));
-        /*
-        int num = 0;
+        System.out.println();
+
+        // Calculating and printing the maximum number
         int max = -10000;
         for(int i = 0; i < 4; i++){
-            num = sc.nextInt();
-            if(max < num)
+            if(max < arr[i]){
+                max = arr[i];
+            }
         }
-        */
+        System.out.print("Максимальное число данного массива: " + max);
     }
 }
