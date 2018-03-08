52c04c7ca3c96ef0f0880fbb199025f142921865
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-08 21:11:51 +0200

    task

---
+++ src/Java_00_Extra/Task_5_1_3.java
@@ -0,0 +1,28 @@
+/*
+Реализуйте метод рисующий на экране прямоугольник из звездочек «*»
+— его параметрами будут целые числа которые описывают длину и
+ширину такого прямоугольника.
+ */
+
+package Java_00_Extra;
+import java.util.Scanner;
+
+public class Task_5_1_3 {
+    public static void main (String []args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите длинну прямоугольника:");
+        int x = sc.nextInt();
+        System.out.println("Введите ширину прямоугольника:");
+        int y = sc.nextInt();
+
+        draw(x,y);
+    }
+    private static void draw(int x, int y){
+        for(int i = 0; i < x; i++){
+            for(int j = 0; j < y; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
