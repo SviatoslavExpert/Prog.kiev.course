a52c29e1003b0131fea4ea73e99f1a43b3ce69f1
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-09 10:49:44 +0200

    task

--- src/Java_00_Extra/Task_5_1_4.java
+++ src/Java_00_Extra/Task_5_1_4.java
@@ -5,6 +5,20 @@
  */
 
 package Java_00_Extra;
+import java.util.Arrays;
 
 public class Task_5_1_4 {
+    public static void main(String[] args) {
+        int[] arr = {11, 45, 69, 101, 350, 12, 539, 999};
+        //System.out.println(Arrays.asList(arr).indexOf("539"));
+        System.out.println(findIndex(arr));
+    }
+    public static int findIndex(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 350) { return i; }
+            else { return -1;}
+        }
+     }
 }
+
+//  for(int i : arr)
