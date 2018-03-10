eaae542885e4685feb205fa013f23a587de498cd
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-10 20:32:53 +0200

    task

--- src/Java_01_Start/task_02_02.java
+++ src/Java_01_Start/task_02_02.java
@@ -10,9 +10,9 @@ package Java_01_Start;
 public class task_02_02 {
 
     public static void main (String []args){
-        double a = 5.5;
-        double b = 4.9;
-        double c = 9.6;
+        double a = 0.3;
+        double b = 0.4;
+        double c = 0.5;
         double p = (a + b + c)/2;
         System.out.println("Площадь треугольника: " + Math.sqrt(p*(p - a)*(p - b)*(p - c)));
     }
