1153e55de818b813c1b5647b6086aea7119829b4
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-11 12:32:22 +0200

    task

--- src/Java_01_Start/task_03_02.java
+++ src/Java_01_Start/task_03_02.java
@@ -24,8 +24,12 @@ public class task_03_02 {
         double radius = 4;
         double result = Math.sqrt(Math.pow((pointX - circleX), 2) + Math.pow((pointY - circleY), 2));
 
-        if (result < radius) System.out.println("Точка лежит внутри круга.");
-        else if(result == radius) System.out.println("Точка лежит на линии круга.");
+        if (result < radius) {
+            System.out.println("Точка лежит внутри круга.");
+        }
+        else if(result == radius) {
+            System.out.println("Точка лежит на линии круга.");
+        }
         else System.out.println("Точка лежит вне круга.");
         sc.close();
     }
