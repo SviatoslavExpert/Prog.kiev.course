e390534da7f5d7e1245f74f260052d86f395394c
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-28 11:24:42 +0300

    task

--- src/Java_00_Extra/TaskOne004.java
+++ src/Java_00_Extra/TaskOne004.java
@@ -1,4 +1,16 @@
 package Java_00_Extra;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class TaskOne004 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Date date = new Date();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd:MM:yyyy");
+    }
+
 }
