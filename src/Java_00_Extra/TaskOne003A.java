74af5f9d77a681b4b60eca5be827314948897d63
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-30 14:51:39 +0300

    task

---
+++ src/Java_00_Extra/TaskOne003A.java
@@ -0,0 +1,13 @@
+package Java_00_Extra;
+
+import java.util.Date;
+
+public class TaskOne003A {
+
+    public static void main(String[] args){
+        Date date = new Date();
+
+        System.out.println(date);
+    }
+
+}
